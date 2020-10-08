259f78b790f7f0302112f51673f9b456b85e3855
Michelle Ranly <meranly8@gmail.com>
2020-10-08 04:44:51 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,17 +1,19 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
 
-class Kid 
-  include Dance 
-  extend MetaDancing
+# class Kid 
+#   include Dance 
+#   extend MetaDancing
   
-  attr_accessor :name 
+#   attr_accessor :name 
   
-  def initialize(name)
-    @name = name
-  end
-end
+#   def initialize(name)
+#     @name = name
+#   end
+# end
 
-class Kid 
-  extend MetaDancing
-end
+# class Kid 
+#   extend MetaDancing
+# end
+
+require_relative './fancy_dance.rb'

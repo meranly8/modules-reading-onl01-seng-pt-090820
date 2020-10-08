f5850a8620d5d918339cba4de954c97af80c08a0
Michelle Ranly <meranly8@gmail.com>
2020-10-08 05:11:28 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,16 +1,16 @@
-# require_relative './class_methods_module.rb'
-# require_relative './dance_module.rb'
+require_relative './class_methods_module.rb'
+require_relative './dance_module.rb'
 
-# class Dancer
-#   include Dance 
-#   extend MetaDancing
+class Dancer
+  include Dance 
+  extend MetaDancing
   
-#   attr_accessor :name 
+  attr_accessor :name 
   
-#   def initialize(name)
-#     @name = name
-#   end
-# end
+  def initialize(name)
+    @name = name
+  end
+end
 
 require_relative './fancy_dance.rb'
 
